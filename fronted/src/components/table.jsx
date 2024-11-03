ee8e83484384d1c44c11c6819979bcaf554deba9
ricardo <eziocard8@gmail.com>
2024-11-03 16:13:24 -0300

    creacion de tabla

--- fronted/src/components/table.jsx
+++ fronted/src/components/table.jsx
@@ -1,24 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
 
 function Table(){
+  const [claves,setClaves] = useState([]); 
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://127.0.0.1:8000/database';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setData(data); // Almacena los datos
+        if (data.length > 0) {
+          setClaves(Object.keys(data[0])); // Obtiene las claves del primer objeto
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return(
   <>
 
   <div>
-  <table class="table">
-  <thead class="table-light">
-    <tr>
-      <th>Titulo 1</th>
-      <th>Titulo 2</th>
-      <th>Titulo 3</th>
-    </tr>
+  <table className="table">
+  <thead className="table-light">
+      <tr>
+          {claves.map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+        </tr>
   </thead>
   <tbody>
-      <tr>
-        <td>Elemento</td>
-        <td>Elemento 2</td>
-        <td>Elemento 3</td>
-      </tr>
+    {data.map((item, index) => (
+            <tr key={index}>
+              {claves.map((key) => (
+                <td key={key}>{item[key]}</td> // Muestra el valor correspondiente a la clave
+              ))}
+            </tr>
+          ))}   
   </tbody>
 </table>
   </div>
